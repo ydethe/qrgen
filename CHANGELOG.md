502a485fa404c6d671759ea1e2931ee2f304448b
Yann de The <ydethe@gmail.com>
2024-09-16 16:53:31 +0200

    Initial work

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -6,3 +6,8 @@ The format is based on [Keep a Changelog](http://keepachangelog.com/en/1.0.0/)
 and this project adheres to [Semantic Versioning](http://semver.org/spec/v2.0.0.html).
 
 <!-- insertion marker -->
+## Unreleased
+
+<small>[Compare with latest](https://github.com/ydethe/qrgen/compare/0fb48dd34c26774884b960a3eb32a5f23abd1357...HEAD)</small>
+
+<!-- insertion marker -->
--- src/qrgen/compute.py
+++ src/qrgen/compute.py
@@ -1,2 +1,42 @@
-def python_add(x: int, y: int) -> int:
-    return x + y
+from flask import render_template
+from flask import Flask
+from flask_bootstrap import Bootstrap
+from flask_wtf import FlaskForm
+from wtforms import StringField
+from wtforms.validators import DataRequired
+from wtforms.fields import SubmitField
+
+
+class MyForm(FlaskForm):
+    url = StringField("url", validators=[DataRequired()])
+    submit = SubmitField("Générer")
+
+
+def create_app(test_config=None):
+    # create and configure the app
+    app = Flask(__name__, instance_relative_config=True)
+    Bootstrap(app)
+    app.config.from_mapping(
+        SECRET_KEY="dev",
+    )
+
+    if test_config is None:
+        # load the instance config, if it exists, when not testing
+        app.config.from_pyfile("config.py", silent=True)
+    else:
+        # load the test config if passed in
+        app.config.from_mapping(test_config)
+
+    # index page
+    @app.route("/", methods=("GET", "POST"))
+    def index():
+        form = MyForm()
+        if form.validate_on_submit():
+            # return redirect('/success')
+            print(form.url)
+        return render_template("index.html", form=form)
+
+    return app
+
+
+# waitress-serve --port 3566 --call 'qrgen.compute:create_app'
