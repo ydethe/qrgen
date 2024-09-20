fe484c7226fac4cfbe2f78d5e8015eaf28964d1c
Yann de The <ydethe@gmail.com>
2024-09-20 10:48:34 +0200

    Added advanced fields

--- src/qrgen/server.py
+++ src/qrgen/server.py
@@ -1,22 +1,102 @@
 import io
+import typing as T
 
 import logfire
 from PIL import Image
 from flask import render_template, Flask, send_file
 from flask_bootstrap import Bootstrap
 from flask_wtf import FlaskForm
-from wtforms import StringField, IntegerField
+from wtforms import StringField, IntegerField, SelectField, ColorField
 from wtforms.validators import DataRequired
 from wtforms.fields import SubmitField
 import qrcode
+from qrcode.image.styledpil import StyledPilImage
+from qrcode.image.styles.moduledrawers.pil import (
+    RoundedModuleDrawer,
+    CircleModuleDrawer,
+    GappedSquareModuleDrawer,
+    HorizontalBarsDrawer,
+    SquareModuleDrawer,
+    VerticalBarsDrawer,
+)
+from qrcode.image.styles.colormasks import (
+    RadialGradiantColorMask,
+    HorizontalGradiantColorMask,
+    SolidFillColorMask,
+    SquareGradiantColorMask,
+    VerticalGradiantColorMask,
+)
 
 from . import settings, logger
 
 
-class MyForm(FlaskForm):
-    url = StringField("Donnée du QR code", validators=[DataRequired()])
-    size = IntegerField("Taille", default=10, validators=[DataRequired()])
+style_choices = {
+    "SquareModuleDrawer": SquareModuleDrawer,
+    "RoundedModuleDrawer": RoundedModuleDrawer,
+    "CircleModuleDrawer": CircleModuleDrawer,
+    "GappedSquareModuleDrawer": GappedSquareModuleDrawer,
+    "HorizontalBarsDrawer": HorizontalBarsDrawer,
+    "VerticalBarsDrawer": VerticalBarsDrawer,
+}
+color_choices = {
+    "SolidFillColorMask": SolidFillColorMask,
+    "RadialGradiantColorMask": RadialGradiantColorMask,
+    "SquareGradiantColorMask": SquareGradiantColorMask,
+    "HorizontalGradiantColorMask": HorizontalGradiantColorMask,
+    "VerticalGradiantColorMask": VerticalGradiantColorMask,
+}
+correction_choices = {
+    "l": qrcode.constants.ERROR_CORRECT_L,
+    "m": qrcode.constants.ERROR_CORRECT_M,
+    "q": qrcode.constants.ERROR_CORRECT_Q,
+    "h": qrcode.constants.ERROR_CORRECT_H,
+}
+
+
+class CodeConfigurationForm(FlaskForm):
+    url = StringField(label="Donnée du QR code", validators=[DataRequired()])
+    size = IntegerField(label="Taille", default=10, validators=[DataRequired()])
+    back_color = ColorField(label="Couleur d'arrière-plan", default="#FFFFFF")
+    fill_color = ColorField(label="Couleur de remplissage", default="#000000")
+    style = SelectField(
+        label="Style",
+        choices=[
+            ("SquareModuleDrawer", "Carrés"),
+            ("RoundedModuleDrawer", "Arrondi"),
+            ("CircleModuleDrawer", "Cercle"),
+            ("GappedSquareModuleDrawer", "Carrés disjoints"),
+            ("HorizontalBarsDrawer", "Barres horizontales"),
+            ("VerticalBarsDrawer", "Barres verticales"),
+        ],
+        default="SquareModuleDrawer",
+    )
+    color = SelectField(
+        label="Couleur",
+        choices=[
+            ("SolidFillColorMask", "Plein"),
+            ("RadialGradiantColorMask", "Gardient radial"),
+            ("SquareGradiantColorMask", "Gardient carré"),
+            ("HorizontalGradiantColorMask", "Gardient horizontal"),
+            ("VerticalGradiantColorMask", "Gardient vertical"),
+        ],
+        default="SolidFillColorMask",
+    )
+    correction = SelectField(
+        label="Correction d'erreur",
+        choices=[
+            ("l", "Bas"),
+            ("m", "Moyen"),
+            ("q", "Bonne qualité"),
+            ("h", "Haute qualité"),
+        ],
+        default="m",
+    )
     submit = SubmitField("Générer")
+
+
+def color_str_to_tuple(col: str) -> T.Tuple[int]:
+    col_t = [int(col[x : x + 2], 16) for x in (1, 3, 5)]
+    return tuple(col_t)
 
 
 def create_app():
@@ -31,22 +111,32 @@ def create_app():
     # Index page
     @app.route("/", methods=("GET", "POST"))
     def index():
-        form = MyForm()
+        form = CodeConfigurationForm()
         if form.validate_on_submit():
             code_data = form.url.data
             code_size = int(form.size.data)
+
+            style_cls = style_choices[form.style.data]
+            color_cls = color_choices[form.color.data]
+            correction = correction_choices[form.correction.data]
+            back_color = color_str_to_tuple(form.back_color.data)
+            fill_color = color_str_to_tuple(form.fill_color.data)
 
             logger.info(f"Generating code for '{code_data}")
 
             qr = qrcode.QRCode(
                 version=code_size,
-                error_correction=qrcode.constants.ERROR_CORRECT_Q,
+                error_correction=correction,
                 box_size=10,
                 border=4,
             )
             qr.add_data(code_data)
             qr.make(fit=False)
-            img = qr.make_image(fill_color="black", back_color="white")
+            img = qr.make_image(
+                image_factory=StyledPilImage,
+                module_drawer=style_cls(),
+                color_mask=color_cls(back_color=back_color, front_color=fill_color),
+            )
             fp = io.BytesIO()
             format = Image.registered_extensions()[".png"]
             img.save(fp, format)
