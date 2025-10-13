2ab1fb39a507a08cee5ba1e24cd63bcc6bd22b3e
Yann de The <ydethe@gmail.com>
2025-10-13 12:05:25 +0200

    blackening

--- tests/test_extension.py
+++ tests/test_extension.py
@@ -4,11 +4,11 @@ from qrgen.server import generate_qrcode
 def test_qrcode_generation():
     generate_qrcode(
         code_size=10,
-    correction="m",
-    code_data="foo",
-    back_color=(255,255,255),
-    fill_color=(0,0,0),
-    style="SquareModuleDrawer",
+        correction="m",
+        code_data="foo",
+        back_color=(255, 255, 255),
+        fill_color=(0, 0, 0),
+        style="SquareModuleDrawer",
     )
 
 
